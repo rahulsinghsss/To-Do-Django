ccd8e52b0016eae13bdc7845010c46a782e23781
Rahul Singh <62008048+rahulsinghsss@users.noreply.github.com>
2022-11-17 08:31:17 +0530

    Add files via upload

---
+++ todo/tasks/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+class Task(models.Model):
+    title=models.CharField(max_length=200)
+    complete=models.BooleanField(default=False)
+    Create=models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.title
